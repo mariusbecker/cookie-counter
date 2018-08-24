5e7c05d98404d191eab6448a30716b0352f1702d
Lars Mäder <mail@larsmaeder.ch>
2018-08-24 21:51:04 +0200

    Push

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let counter = document.getElementById("counter");
 let points = 0;
 var intervalID = window.setInterval(subtractpoints, 500);
 var timeoutID = window.setTimeout(bonusevent, 1000);
-
+let bonuselement = document.getElementById("bonuselement");
 
 
 cookie.addEventListener("click", clicker);
@@ -15,7 +15,7 @@ function clicker() {
         points = points + 10;
     }
     else {
-        points = points + 1;
+        points = points + 5;
     }
 }
 
@@ -29,3 +29,17 @@ function subtractpoints() {
 function bonusevent() {
     cookie2.classList.remove("hidden");
   }
+
+  function getRandomPosition(element) {
+	var x = document.body.offsetHeight-element.clientHeight;
+	var y = document.body.offsetWidth-element.clientWidth;
+	var randomX = Math.floor(Math.random()*x);
+	var randomY = Math.floor(Math.random()*y);
+	return [randomX,randomY];
+}
+window.onload = function() {
+	document.body.appendChild(bonuselement);
+	var xy = getRandomPosition(bonuselement);
+	bonuselement.style.top = xy[0] + 'px';
+	bonuselement.style.left = xy[1] + 'px';
+} 
