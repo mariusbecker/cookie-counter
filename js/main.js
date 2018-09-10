a5124acfce1637afafea8a29003fcba6060464b1
Lars Mäder <mail@larsmaeder.ch>
2018-09-10 22:00:30 +0200

    ADD: better substraction method; multiplier; 2x style

--- js/main.js
+++ js/main.js
@@ -1,29 +1,40 @@
 let cookie = document.getElementById("cookie");
 let counter = document.getElementById("counter");
-var intervalID = window.setInterval(subtractpoints, 500);
 let bonuselement = document.getElementById("bonuselement");
 let gameover = document.getElementById("gameover");
 let pressplay = document.getElementById("pressplay");
 let points = 0;
 var t;
+var subtractinterval;
+var factor = 1;
 
 cookie.addEventListener("click", clicker);
 cookie.addEventListener("click", gameoverscreen);
 cookie.addEventListener("click", startscreen);
-bonuselement.addEventListener("click", pointsplus200);
-$("#cookie").one("click", startgame);
+cookie.addEventListener("mousedown", pausesubtractpoints);
+cookie.addEventListener("mouseup", runsubtractionpoints);
+bonuselement.addEventListener("click", pointsx2);
+$("#cookie").one("click", startgame); //jquery event to run a function only one single time
 
 function clicker() {
     counter.innerHTML = points;
     if(points >= 200) {
-        points = points + 20;
+        points = points + (3*factor);
     }
     if(points >= 100) {
-        points = points + 10;
+        points = points + (2*factor);
     }
     else {
-        points = points + 1;
+        points = points + (1*factor);
     }
+}
+
+function pausesubtractpoints(){
+    clearInterval(subtractinterval);
+}
+
+function runsubtractionpoints(){
+    subtractinterval = window.setInterval(subtractpoints, 500);
 }
 
 function startgame() {
@@ -47,10 +58,10 @@ function startscreen() {
 function subtractpoints() {
     counter.innerHTML = points;
     if(points >= 200) {
-        points = points - 20;
+        points = points - 9;
     }
     if(points >= 100) {
-        points = points - 10;
+        points = points - 6;
     }
     if(points >= 1) {
         points = points - 1;
@@ -63,9 +74,15 @@ function bonusevent() {
 	var xy = getRandomPosition(bonuselement);
 	bonuselement.style.top = xy[0] + 'px';
     bonuselement.style.left = xy[1] + 'px';
-    setTimeout(function(){ bonuselement.classList.add("hidden"); }, 1000);
+    setTimeout(function(){ bonuselement.classList.add("hidden"); }, 5000);
   }
 
+  function pointsx2() {
+    setTimeout(function(){ factor = 2; }, 1);
+    setTimeout(function(){ factor = 1; }, 5000);
+    bonuselement.classList.add("hidden");    
+  } 
+ 
   function pointsplus200() {
     points = points + 200;
     counter.innerHTML = points;
