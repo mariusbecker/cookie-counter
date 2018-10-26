8604908d4f8ba07447949f75fbc5a99ca982a72f
Lars Mäder <mail@larsmaeder.ch>
2018-10-26 14:30:43 +0000

    Deploy larsmaeder/cookie-counter to github.com/larsmaeder/cookie-counter.git:gh-pages

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
-let cookie = document.getElementById("cookie");
-let counter = document.getElementById("counter");
-let bonuselement = document.getElementById("bonuselement");
-let gameover = document.getElementById("gameover");
-let pressplay = document.getElementById("pressplay");
-let pluspoints = document.getElementById("pluspoints");
-let minuspoints = document.getElementById("minuspoints");
-let points = 0;
+var cookie = document.getElementById("cookie");
+var counter = document.getElementById("counter");
+var bonuselement = document.getElementById("bonuselement");
+var gameover = document.getElementById("gameover");
+var pressplay = document.getElementById("pressplay");
+var pluspoints = document.getElementById("pluspoints");
+var minuspoints = document.getElementById("minuspoints");
+var progressbar = document.getElementById("progressbar");
+var points = 500;
 var t;
 var a;
 var b = 1;
-var c = 1;
 var count;
 var subtractinterval;
 var factor = 1;
 var varpoint = 1;
 
-// Stopwatch
+// Stopwatch variables
 var time = document.getElementsByClassName("stopwatch"),
 c=0, s=0, m=0, io=0, itv=null;
 
+// Eventlisteners
 cookie.addEventListener("click", clicker);
 cookie.addEventListener("mousedown", mousedown);
 cookie.addEventListener("mouseup", mouseup);
-bonuselement.addEventListener("click", pointsx2);
-//run only one single time
+bonuselement.addEventListener("click", pointsplus100);
+
+//Run only one single time
 $("#cookie").one("click", startgame);
 $("#cookie").one("click", startscreen);
 $("#cookie").one("click", gameoverscreen); 
@@ -32,6 +34,7 @@ $("#cookie").one("click", playPause);
 function clicker() {
     counter.innerHTML = points;
     pluspoints.innerHTML = b; 
+    progressbar.value = points;
     points = b + points; 
     if(points >= 300) {
         b = 4;
@@ -61,10 +64,10 @@ function mouseup(){
 }
 
 function startgame() {
-    t = setTimeout(bonusevent, 2000);
-    points = 2;
+    t = setInterval(bonusevent, 6000);
+    points = 500;
     minuspoints.innerHTML = varpoint;
-    a = window.setInterval(function test(){
+    a = window.setInterval(function(){
         varpoint = varpoint + 1;
         minuspoints.innerHTML = varpoint;
         console.log(varpoint);
@@ -101,14 +104,8 @@ function bonusevent() {
     setTimeout(function(){ bonuselement.classList.add("hidden"); }, 5000);
   }
 
-function pointsx2() {
-    setTimeout(function(){ c = 2; }, 1);
-    setTimeout(function(){ c = 1; }, 5000);
-    bonuselement.classList.add("hidden");    
-} 
- 
-  function pointsplus200() {
-    points = points + 200;
+  function pointsplus100() {
+    points = points + 100;
     counter.innerHTML = points;
     bonuselement.classList.add("hidden");    
   }
@@ -121,6 +118,8 @@ function pointsx2() {
 	return [randomX,randomY];
 }
 
+
+// Stopwatch functions
 function padd(n) { // Zero padd minutes and seconds
     return n<10 ? "0"+n : n;
   }
@@ -131,11 +130,13 @@ function count() {
         s = ++s%60;
       if(!s) m=++m%60;
     }
-    time[0].innerHTML = padd(m) +":"+ padd(s) +"."+ padd(c);
-    time[1].innerHTML = padd(m) +":"+ padd(s) +"."+ padd(c);
+    time[0].innerHTML = padd(m) +":"+ padd(s) +":"+ padd(c);
+    time[1].innerHTML = padd(m) +":"+ padd(s) +":"+ padd(c);
   }
 
   function playPause() {
     io = !io;
     return io ? itv = setInterval(count, 10) : clearInterval(itv);
   }
+
+//   points = maths.min(100/5);
